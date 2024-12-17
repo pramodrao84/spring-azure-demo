03fbed1bd52f20511c324821174602739eeb9a77
Pranitha-cap <pranithaiiit@gmail.com>
2024-12-16 20:41:39 -0500

    deploying microservice to azure

---
+++ src/main/java/com/microservices/demo/TestMicroserviceOnAzureApplication.java
@@ -0,0 +1,14 @@
+package com.microservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMicroserviceOnAzureApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TestMicroserviceOnAzureApplication.class, args);
+	}
+
+}
